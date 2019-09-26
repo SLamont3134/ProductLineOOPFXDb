3f8daa3f08de0f8fad2ec302decd1133148ad9f4
SLamont <stlamont3134@eagle.fgcu..edu>
2019-09-26 16:14:43 -0400

    Week 4 Complete

--- src/lamont/Controller.java
+++ src/lamont/Controller.java
@@ -1,4 +1,38 @@
 package lamont;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+
+  @FXML
+  private Button addButton;
+
+  @FXML
+  private TableView<?> existingProductWindow;
+
+  @FXML
+  private ComboBox<?> chooseQtyBox;
+
+  @FXML
+  private Button recordProductionBttn;
+
+  @FXML
+  private TextArea productionLogTextArea;
+
+  @FXML
+  void addButtonAction(ActionEvent event) {
+    System.out.println("Add Button Pressed");
+  }
+
+  @FXML
+  void recordProductionBttnAction(ActionEvent event) {
+    System.out.println("Record Production Button Pressed");
+
+  }
+
 }
--- src/lamont/Main.java
+++ src/lamont/Main.java
@@ -1,5 +1,10 @@
 package lamont;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,14 +26,37 @@ public class Main extends Application {
    * @param primaryStage the primary stage
    * @throws Exception  Any problems with code
    */
+  private Statement statement;
     @Override
     public void start(Stage primaryStage) throws Exception{
+        connectToDB();
         Parent root = FXMLLoader.load(getClass().getResource("OOPProject.fxml"));
         primaryStage.setTitle("OOPProject");
         //primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
+  private void connectToDB() {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/ProductionDB";
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+
+    try {
+
+      Class.forName(JDBC_DRIVER);
+
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+
+    }catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
 
 
     public static void main(String[] args) {
