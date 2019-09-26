18b61078595725330d1e71d2bca91369ea7ed3c5
SLamont <stlamont3134@eagle.fgcu..edu>
2019-09-26 17:32:15 -0400

    Sprint 1 Complete. Needs debugging

--- src/lamont/Controller.java
+++ src/lamont/Controller.java
@@ -1,38 +1,101 @@
+/**
+ * @author Sean Lamont
+ */
+
 package lamont;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 
-public class Controller {
 
-  @FXML
-  private Button addButton;
+/**
+ * @author Sean Lamont
+ * @brief The Controller class, Controls the GUI and calls all methods.
+ * @throws Exception ex, ClassNotFoundException
+ */
+public class Controller implements Initializable {
 
-  @FXML
-  private TableView<?> existingProductWindow;
+  static final String JDBC_DRIVER = "org.h2.Driver";
+  static final String DB_URL = "jdbc:h2:./res/ProductionDB";
+  static final String USER = "SeanLamont";
+  static final String PASS = "password";
+  Connection conn = null;
 
-  @FXML
-  private ComboBox<?> chooseQtyBox;
+  @FXML private Button addButton;
 
-  @FXML
-  private Button recordProductionBttn;
+  @FXML private TableView<?> existingProductWindow;
 
-  @FXML
-  private TextArea productionLogTextArea;
+  @FXML private ComboBox<Integer> chooseQtyBox;
 
+  @FXML private Button recordProductionBttn;
+
+  @FXML private TextArea productionLogTextArea;
+
+
+  /**
+   * @brief The addButtonAction this method handle the event of the addButton being pressed.
+   * @return void
+   * @param event
+   */
   @FXML
   void addButtonAction(ActionEvent event) {
+    Statement statement;
+
     System.out.println("Add Button Pressed");
+
+    try {
+      // Main.connectToDB();
+
+      Class.forName(JDBC_DRIVER);
+
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      statement = conn.createStatement();
+      String sql =
+          "INSERT INTO Product(type, manufacturer, name) VALUES " + "( 'AUDIO', 'Apple', 'iPod' );";
+      statement.execute(sql);
+
+      statement.close();
+      conn.close();
+
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
   }
 
+  /**
+   * @brief The recordProductionButton this method handle the event of the
+   * productionButton being pressed.
+   * @return void
+   * @param event
+   */
   @FXML
   void recordProductionBttnAction(ActionEvent event) {
     System.out.println("Record Production Button Pressed");
-
   }
 
+  /**
+   * @brief The addButtonAction this method handle the event of the addButton being pressed.
+   * @return void
+   * @param location, resources
+   */
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    chooseQtyBox.getItems().addAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    chooseQtyBox.setEditable(true);
+    chooseQtyBox.getSelectionModel().selectFirst();
+  }
 }
--- src/lamont/Main.java
+++ src/lamont/Main.java
@@ -1,9 +1,12 @@
+/**
+ * @author Sean Lamont
+
+ */
+
 package lamont;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,37 +14,43 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * @author Sean Lamont
+/** @author Sean Lamont
+ * @desc The main method, establishes communication with the database.
  */
 
-//be sure to do analyze inspect code
+// be sure to do analyze inspect code
 
-//also use findbugs
+// also use findbugs
 public class Main extends Application {
 
   /**
    * the start method is the starting point of a JavaFX program. The start method sets the title.
+   *
    * @brief
    * @param primaryStage the primary stage
-   * @throws Exception  Any problems with code
+   * @throws Exception Any problems with code
    */
   private Statement statement;
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        connectToDB();
-        Parent root = FXMLLoader.load(getClass().getResource("OOPProject.fxml"));
-        primaryStage.setTitle("OOPProject");
-        //primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
 
-  private void connectToDB() {
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    connectToDB();
+    Parent root = FXMLLoader.load(getClass().getResource("OOPProject.fxml"));
+    primaryStage.setTitle("OOPProject");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
+  }
+
+  /**
+   * @author Sean Lamont
+   * @brief The connect to database method. Establishes connection to ProductionDB H2 Database.
+   * @throws Exception ex, ClassNotFoundException
+   */
+  protected static void connectToDB() {
     final String JDBC_DRIVER = "org.h2.Driver";
     final String DB_URL = "jdbc:h2:./res/ProductionDB";
-    final String USER = "";
-    final String PASS = "";
+    final String USER = "SeanLamont";
+    final String PASS = "password";
     Connection conn = null;
 
     try {
@@ -49,17 +58,25 @@ public class Main extends Application {
       Class.forName(JDBC_DRIVER);
 
       conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      // statement = conn.createStatement();
+
+      System.out.println("Connected to DB successfully");
+
+      conn.close();
 
     } catch (ClassNotFoundException e) {
       e.printStackTrace();
 
-    }catch (Exception ex) {
+    } catch (Exception ex) {
       ex.printStackTrace();
     }
   }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  /**
+   * @brief The main method, simply launches the program.
+   * @param args
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
